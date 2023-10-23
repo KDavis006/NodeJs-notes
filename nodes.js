fe6d655d7378955c7c3052b86527a3a216af72ef
KDavis006 <110576696+KDavis006@users.noreply.github.com>
2023-10-23 13:43:55 -0700

    created the node.jsfile

---
+++ nodes.js
@@ -0,0 +1,6 @@
+// Kyle Davis, Brianne simeson, Ren Levingar, Andrew Silahi
+
+// 33: Open JS Foundation
+// Node.js Foundation and the JS Foundation merged to form the OpenJS Foundation. The OpenJS Foundation is made up of 32 open source JavaScript projects including Appium, Dojo, jQuery, Node.js, and webpack. Their mission is to support the healthy growth of JavaScript and web technologies by providing a neutral organization to host and sustain projects, as well as collaboratively fund activities that benefit the ecosystem as a whole.
+
+
