9d28318424259745fae55b1f8663f10c21214a01
KDavis006 <110576696+KDavis006@users.noreply.github.com>
2023-10-23 14:52:40 -0700

    Update app.js

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-// Kyle Davis, Brianne Simesen, Ren Levingar, Andrew Silahi
+// Kyle Davis, Brianne Siemsen, Ren Levingar, Andrew Silahi
 
 // 33: Open JS Foundation
 // Node.js Foundation and the JS Foundation merged to form the OpenJS Foundation. The OpenJS Foundation is made up of 32 open source JavaScript projects including Appium, Dojo, jQuery, Node.js, and webpack. Their mission is to support the healthy growth of JavaScript and web technologies by providing a neutral organization to host and sustain projects, as well as collaboratively fund activities that benefit the ecosystem as a whole.
